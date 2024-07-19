3c67d1468abbc4ebf8347b3eaab0c762a84f648d
Xiaoyang Jia <77788411+anfwr@users.noreply.github.com>
2024-07-19 04:44:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  var member = document.getElementById("member").value;
+  var member = member.toLowerCase();
+  var member = member.trim();
+  if (member) {
+    // Add your code here
+  }
+}
